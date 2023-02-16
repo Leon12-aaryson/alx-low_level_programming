2261d5c868e3a8916a879963c72b86ebed29810c
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-02-16 12:48:52 -0800

    Automated updates to c-low level pregramming assignment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include<stdio.h>
 /**
- * the betty  comment style
- * it actually prompts you to use main(void)
- * description: "\"Programming is like building a multilingual puzzle"
- * puts is an output dunction preffered in the number to printf
- *stdio stands for standard input output
+ * main - Entry point
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
 int main(void)
 {
