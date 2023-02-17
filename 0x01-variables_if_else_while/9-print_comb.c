72da369b40f03dbdd4a258ccd15bc19941322d08
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-02-17 15:54:33 -0800

    This file is for c loop variables

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,20 +1,25 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <unistd.h>
 /**
- * main - Prints all possible combinations of single digits
- * Return: Always return 0
- *
+ * main - Entry point
+ * Description: prints numbers
+ * Return: Always 0 (success)
  */
 int main(void)
 {
-	int num;
-	for (num=0; num<=9; num++)
+	int i;
+
+	for (i = '0'; i <= '9' ; i++)
 	{
-		putchar((num%10) + '0');
-		if(num==9)
-			continue;
-		putchar(',');
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
+
 	putchar('\n');
-	return(0);
+	return (0);
 }
