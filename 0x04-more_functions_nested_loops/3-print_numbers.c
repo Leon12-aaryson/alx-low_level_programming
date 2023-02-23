58094485b4dcae0552906e4d6c0d950e9dda05f4
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-02-23 15:14:00 -0800

    This file commits are for 0x04 assignment low level programming on functions

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers: print numbers 0-9
+ * print_numbers - print  0 - 9
  *
  * Return: void
  */
+
 void print_numbers(void)
 {
-	int a = 0;
+	char a = 0;
 
 	while (a <= 9)
 	{
-		_putchar(a);
+		_putchar(a + '0');
 		a++;
 	}
 	_putchar('\n');
