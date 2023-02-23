f27f36654bb3fab632f1dded4450b988597fbfcd
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-02-23 12:05:42 -0800

    This file commits are for 0x04 assignment low level programming on functions

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isdigt - checks is a character is uppercase
+ *
+ * Return: returns 1 if true else 0 if false
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 0)
+	{
+		return (1);
+	}
+	return (0);
+}
