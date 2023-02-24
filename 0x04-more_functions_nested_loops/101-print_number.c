db5cd16c76081fe7b7ec0f28d1f39d818918f8cd
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-02-24 12:09:02 -0800

    This file commits are for 0x04 assignment low level programming on functions

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * main - print integers
+ *
+ * @n: number to print
+ * Return: always 0
+ */
+
+void print_number(int n)
+{
+	int x;
+
+	x = n;
+
+	if (n < 0)
+	{
+		_putchar(20);
+		x = -n;
+	}
+	if (x / 10)
+		print_number(x / 10);
+	_putchar ((x / 10) + '0');
+}
+
