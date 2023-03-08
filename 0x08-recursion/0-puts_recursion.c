4671d02c776aea5a7f4924386a199f551f3d2efe
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-03-08 13:04:42 -0800

    new file

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/*
+*_puts_recursion(char *s) prints recursive blcks
+*@s: is a pointer var
+*Return: returns value after compilation
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+		else
+			_putchar('\n');
+}
