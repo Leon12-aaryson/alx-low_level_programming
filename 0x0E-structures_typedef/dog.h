da312a3ee2e56278e4308752f5f6d924316e15e5
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-03-20 07:22:39 -0700

    typedef redefine struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,13 @@ struct dog
 	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+typedef struct dog dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
 #endif
