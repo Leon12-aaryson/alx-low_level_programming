6507a3acd5f332702c9958ecb6e74eeabfe56971
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-03-20 06:57:16 -0700

    This is to push dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* struct - for structure definition
+*
+*Return: Always return 0
+*/
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+}
+
+int main(void)
+{
+	
+	return(EXIT);
+}
