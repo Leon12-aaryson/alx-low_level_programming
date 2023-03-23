5ab0d7307ff132ee5410b844fcdb7cab2422cc1a
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-03-23 13:32:59 -0700

    error correction or header file

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,19 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ * struct printff - A new struct type defining a printff.
+ * @symbol: Datatype symbol.
+ * @print: function pointer.
+ */
+
+typedef struct printff
+{
+	char *symbol;
+	void (*print)(va_list args);
+
+} f;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
