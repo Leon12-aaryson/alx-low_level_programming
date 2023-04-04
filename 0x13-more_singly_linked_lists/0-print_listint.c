c604b836dcf8a6ef378894847e03a3d49263ed0d
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-04 15:46:45 -0700

    new update to sem

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,18 +5,20 @@
 *print_listint - this fundtion prints a list of nodes in the linked list
 *
 *@h: member pointer of a struct
+*
 *Return: Number of nodes
 */
 
 size_t print_listint (const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t sem = 0;
 
 	while (h)
 	{
-		nodes++;
+		sem++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (nodes);
+
+	return (sem);
 }
