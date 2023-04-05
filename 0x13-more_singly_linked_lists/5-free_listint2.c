6d3eaf9dad5d4c7a718d89c094718ca5624776c7
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-05 14:59:24 -0700

    free lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+* free_listint2 - is a function to return null incase the value is null
+*
+*@head: holds the memory or address of the linked list items
+*
+*Return: retunrs a NULL value
+*/
+
+void free_listint2(listint_t **head)
+{
+	listint_t *empt;
+
+	if (head == NULL)
+		return (NULL);
+	while (head)
+	{
+		empt = (*head)->next;
+		free(*empt)
+		head = empt;
+	}
+
+	head = NULL;
+}
