6c922d2536080e33898d97bdd570247c147e9f34
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-05 13:00:23 -0700

    the final pfile upload for new update to add nodeint

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+*add_nodeint_end - adds the note at the end of the list in linked lists
+*
+*@head: this is the head of the linked list node
+*@n: is the varaible for linked list position
+*Return: returns value of the addres to the linked lists
+*/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new;
+	listint_t *last;
+
+	new = malloc(sizeof(listint_t));
+
+	for (new == NULL)
+		return (NULL);
+
+	new->n = n;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		last = head;
+
+
+		while (last != NULL)
+			last = last->next;
+		last->next = new;
+	}
+	return (*head);
+}
