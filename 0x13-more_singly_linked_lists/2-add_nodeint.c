63658130912147f3b6af31979caa18a67f182313
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-05 08:10:16 -0700

    2 new location

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+* add_nodeint - function to add new nodes
+*
+*@n: constant integer declaratin on function
+*@head: is standing in fr new mem location f linked list items
+*
+*Return: Retuns the new value of linked lisy
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizef(listint_t));
+
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
