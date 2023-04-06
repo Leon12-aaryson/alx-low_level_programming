6acdb319b97a063d8fa15c15764eac9c7a8a64a5
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-06 03:38:01 -0700

    rectifying the betty error in the code

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
+*get_nodeint_at_index - defines how to get the nth node of the linked list
 *
-*
-*
-*
-*
+*@head: is the head of the node in the linked list
+*@index: is the index of the pointer of a node
+*Return: returns 1 when program runs successfully and -1 when it doesnt
 */
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
