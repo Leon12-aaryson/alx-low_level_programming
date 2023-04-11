a1e6baa8f2aa6d186246d561fbff4a766644c911
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-11 10:27:56 -0700

    null value

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,9 +10,12 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0;
-	int len = 0;
+	unsigned int len = 0;
 
 	if (b[len] == '\0')
+		return (0);
+
+	if (b == NULL)
 		return (0);
 
 	while ((b[len] == '0') || (b[len] == '1'))
