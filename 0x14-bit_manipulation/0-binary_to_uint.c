4f43ddb3c99c05a2e0f8ead2c8fe6fa74dd36068
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-04-11 10:43:49 -0700

    another null trial check

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,10 +12,9 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int i = 0;
 	unsigned int len = 0;
 
-	if (b[len] == '\0')
-		return (0);
-
 	if (b == NULL)
+		return (0);
+	if (b[len] == '\0')
 		return (0);
 
 	while ((b[len] == '0') || (b[len] == '1'))
