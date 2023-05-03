e94d1f0d751017cba7265a37a47b2b14224cb231
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-05-03 15:37:48 -0700

    add another

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * pop_listint - this function returns a node saved in the lists
+ * @head: first node argument in the function
+ *
+ * Return: returns stored values
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *list_new;
+	int val;
+
+	if (!head)
+		return (0);
+	if (*head == NULL)
+		return (0);
+
+	val = (*head)->n;
+	if ((*head)->next == NULL)
+	{
+		list_new= *head;
+		free(list_new);
+		*head = NULL;
+		return (val);
+	}
+
+	list_new = *head;
+	*head = (*head)->next;
+	free(list_new);
+
+	return (val);
+}
