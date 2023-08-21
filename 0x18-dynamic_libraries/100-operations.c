fba0383c16bd6b6caac9290124918d29e890a85a
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-08-21 13:31:33 -0700

    final function

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+/**
+ * add - adds two integer values provided
+ * @a: integer value a
+ * @b integer value b
+ *
+ * Return: sum (success)
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
+
+/**
+ * sub - function the gets difference
+ * @a: integer
+ * @b: integer
+ *
+ * Return: sub (success)
+ */
+int sub(int a, int b)
+{
+	int sub;
+
+	sub = a - b;
+
+	return (sub);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: sub (success)
+ */
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+
+	return (mul);
+}
+
+/**
+ * mul - multiplies two integers
+ * @a: integer
+ * @b: integer
+ *
+ * Return: div (success)
+ */
+int div(int a, int b)
+{
+	int div;
+
+	div = a / b;
+
+	return (div);
+}
+
+/**
+ * mod - finds remainder of divison
+ * @a: integer
+ * @b: integer
+ *
+ * Return: mod
+ */
+int mod(int a, int b)
+{
+	int mod;
+
+	mod = a % b;
+
+	return (mod);
+}
