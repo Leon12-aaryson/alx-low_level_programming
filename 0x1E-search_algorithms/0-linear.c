77326ef222cc8b6634a2f289045695daf55f66ce
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-12-13 16:44:54 +0300

    new update on betty format

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 
 
 /**
-* linear-search: this is the function to search integer values
+* linear_search - this is the function to search integer values
 * @array: array list to be searched
 * @size: size of array
 * @value: value being searched
@@ -12,23 +12,23 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL)
-    {
-        printf("Array is NULL\n");
-        return (-1);
-    }
+	if (array == NULL)
+	{
+		printf("Array is NULL\n");
+		return (-1);
+	}
 
-    for (size_t i = 0; i < size; ++i)
-    {
-    printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+	for (size_t i = 0; i < size; ++i)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        if (array[i] == value)
-        {
-            printf("Found %d at index: %lu\n", value, i);
-            return (i);
-        }
-    }
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %lu\n", value, i);
+			return (i);
+		}
+	}
 
-    printf("Value %d not found in the array\n", value);
-    return (-1);
+	printf("Value %d not found in the array\n", value);
+	return (-1);
 }
