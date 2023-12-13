c2b7b7ebccecdceff109b5740d46b33d1cd4b3ec
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-12-13 16:38:10 +0300

    new search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,35 +1,34 @@
-#include <stdio.h>
+#include "search_algos.h"
 
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
+
+/**
+* linear-search: this is the function to search integer values
+* @array: array list to be searched
+* @size: size of array
+* @value: value being searched
+*
+* Return: return value
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    if (array == NULL)
+    {
         printf("Array is NULL\n");
-        return -1; // Array is NULL, return -1
+        return (-1);
     }
 
-    for (size_t i = 0; i < size; ++i) {
-        printf("Comparing %d with %d\n", array[i], value);
+    for (size_t i = 0; i < size; ++i)
+    {
+    printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
-        if (array[i] == value) {
-            return i; // Value found, return the index
+        if (array[i] == value)
+        {
+            printf("Found %d at index: %lu\n", value, i);
+            return (i);
         }
     }
 
-    return -1; // Value not found in the array
-}
-
-int main() {
-    // Example usage
-    int arr[] = {10, 5, 7, 2, 8};
-    size_t size = sizeof(arr) / sizeof(arr[0]);
-    int search_value = 7;
-
-    int result = linear_search(arr, size, search_value);
-
-    if (result != -1) {
-        printf("Value %d found at index %d\n", search_value, result);
-    } else {
-        printf("Value %d not found in the array\n", search_value);
-    }
-
-    return 0;
+    printf("Value %d not found in the array\n", value);
+    return (-1);
 }
