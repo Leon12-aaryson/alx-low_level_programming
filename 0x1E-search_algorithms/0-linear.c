4cdcf441c358f293a4a9295af7230185c7e82c99
leon12-aaryson <aaronoluk4deleonardo@gmail.com>
2023-12-14 20:01:39 +0300

    create new update on error

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,36 +1,66 @@
 #include "search_algos.h"
-
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* linear_search - this is the function to search integer values
-* @array: array list to be searched
-* @size: size of array
-* @value: value being searched
-*
-* Return: return value
-*/
+ * linear_search - Function to search for a value in an array.
+ * @array: Pointer to the array to be searched.
+ * @size: Size of the array.
+ * @value: Value to be searched.
+ *
+ * Return:
+ *   - Index of the first occurrence of the value (for one occurrence).
+ *   - Array of indices of occurrences (for multiple occurrences).
+ *   - -1 if the value is not found.
+ */
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+	size_t i, count = 0;
+	int *indices = NULL;
+	int found = 0;
 
 	if (array == NULL)
 	{
 		printf("Array is NULL\n");
 		return (-1);
 	}
-
 	for (i = 0; i < size; ++i)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-
 		if (array[i] == value)
 		{
-			printf("Found %d at index: %lu\n", value, i);
-			return (i);
+			if (!found)
+			{
+				printf("Found %d at index: %lu\n", value, i);
+				found = 1;
+			}
+
+			indices = (int *)realloc(indices, (count + 1) * sizeof(int));
+
+			if (indices == NULL)
+			{
+				printf("Memory allocation failed\n");
+				exit(EXIT_FAILURE);
+			}
+			indices[count++] = i;
 		}
 	}
-
-	printf("Value %d not found in the array\n", value);
-	return (-1);
+	if (!found)
+	{
+		printf("Value %d not found in the array\n", value);
+		free(indices);
+		return (-1);
+	}
+	if (count > 1)
+	{
+		printf("Found %d at indices:", value);
+		for (i = 0; i < count; ++i)
+		{
+			printf(" %d", indices[i]);
+		}
+		printf("\n");
+		return (-1);
+	}
+	return (indices[0]);
 }
